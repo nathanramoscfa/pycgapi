b31ea83646eec8e720b65666eab863016f0b4131
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2024-01-09 17:03:54 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-from pycgapi.__version__ import __version__
+from __version__ import __version__
 
 # -- Project information -----------------------------------------------------
 
